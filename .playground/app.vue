232c140ba2e7f6c7b8cc01a93feaae7435669ce2
Thomas Fjordside <48123373+tfjordside@users.noreply.github.com>
2023-02-20 16:00:47 +0100

    fix: pagination changes (#1)

--- .playground/app.vue
+++ .playground/app.vue
@@ -2,14 +2,33 @@
   <div>
     Hello World!
 	<LimboSearch :search-filters="{
-			endpointUrl: 'https://www.slagelse.dk/api/news/search/',
-			fields: [{name: 'contextId', value: '1248'}, {name: 'text', value: 'test'}, {name:'limit', value: '16'}],
+			endpointUrl: 'https://www.jammerbugt.dk/api/jobs/search/',
+			fields: [{name: 'contextId', value: '1588'}, {name: 'text', value: ''}, {name:'limit', value: '16'}],
 		}"
-		#default="{ data, pagination }"
+		#default="{ data, pagination, action }"
 	>
-		<pre>{{ data }}</pre>
-		<pre>{{ pagination }}</pre>
-
+		<template v-if="data">
+			<pre>{{ pagination }}</pre>
+			<pre>{{ data }}</pre>
+			<div
+				v-if="
+					pagination.total -
+						pagination.limit -
+						pagination.offset >
+						0
+				"
+				class="flex justify-center"
+			>
+				<button
+					@click="action.fetchMore()"
+					class="mt-60 bg-purple text-white spa-link cursor-pointer flex items-center justify-center py-20 px-48 text-md hover:bg-purple-light transition duration-200 ease-smooth-line spa-link--prefetched"
+				>
+					<!-- eslint-disable -->
+					<span slot="label">Vis flere</span>
+					<!-- eslint-enable -->
+				</button>
+			</div>
+		</template>
 	</LimboSearch>
   </div>
 </template>
